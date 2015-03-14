ba051f5a660fec6397104231e815b0872ebc6552
tlembke <tony@lemlink.com.au>
2015-03-14 11:48:07 +1100

    Completed add_condition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :xeditable?
 
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -9,5 +10,7 @@ class ApplicationController < ActionController::Base
   def xeditable? object=nil
     true # Or something like current_user.xeditable?
   end
+
+
   
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -7,5 +7,46 @@ class Person < ActiveRecord::Base
     has_many :measuresvalues
 
     validates :family_name, :given_names, presence: true
-    
-end
+
+    def family_name_given_names
+   		if self.given_names.blank?
+     		return self.family_name
+   		else
+     		return self.family_name+", "+self.given_names
+   		end
+ 	end
+
+ 	def full_name
+   		if self.given_names.blank?
+     		return self.family_name
+  		 else
+     		return self.given_names+" "+self.family_name
+   		end
+ 	end
+
+ 	def full_name_known_as
+ 		r=self.given_names
+ 		r=r+" " unless r.blank?
+ 		r=r+"'"+self.known_as+"' " unless self.known_as.blank? or self.known_as==self.given_names
+   		r=r+self.family_name
+   		return r
+ 	end
+
+    def address
+      	address= self.address_line
+    	address+=", " if self.address_line!=""
+    	address+=self.town
+  	end
+
+  	def full_address
+    	r = address_line
+    	r+="\n" unless address_line.blank?
+    	r+= town+" "+postcode
+    	return r
+  	end
+
+  	def gender
+  		self.sex=0? r="Male" : r="Female"
+  		return r
+  	end
+ end
